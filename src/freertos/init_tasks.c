ec457849b5ce016ef76f52d303e7e46e61cad4df
scoltharp <steven1coltharp@gmail.com>
2026-03-10 11:40:41 -0500

    Revert"Created task to test ISR, REVERT CHANGES LATER!!!"
    This reverts commit 8bc3890f0809fafbaab9f14d0bdf76524b97500f.

--- src/freertos/init_tasks.c
+++ src/freertos/init_tasks.c
@@ -3,13 +3,11 @@
 #include "freertos/task_can.h"
 #include "freertos/task_housekeeping.h"
 #include "freertos/task_rx.h"
-#include "freertos/task_irq.h"
 
 void init_tasks(void) {
     launch_radio_task();
     launch_can_task();
     launch_housekeeping_task();
     launch_rx_task();
-    launch_isr_test_task();
 
 }
